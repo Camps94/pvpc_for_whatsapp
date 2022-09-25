b2f642f40b698a5c7a4eef789e10a6170b05113f
Camps94 <eduardocampillos@Eduardos-MacBook-Pro.local>
2022-09-25 22:02:50 +0100

    addsa

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def sms_reply():
     resp = MessagingResponse()
     if msg == "PVPC":
         resp.message = ("OKEEEEEY")
-        #print(get_pvpc())
+        print(get_pvpc())
     else:
         resp.message("Hi, You said: {}. If you want to get the PVPC for tommorow, reply PVPC. ".format(msg))
 
