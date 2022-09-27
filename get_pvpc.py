0dfe87e394b56c9f1cf9d8f20821f689679ce9c5
Camps94 <eduardocampillos@Eduardos-MacBook-Pro.local>
2022-09-28 00:39:33 +0100

    To be continued

--- get_pvpc.py
+++ get_pvpc.py
@@ -337,7 +337,7 @@ def get_pvpc_results():
     print("#####")
     print(df.columns)
     for index, row in df.iterrows():
-        index = index.strftime("%d/%m/%Y, %H:%M")
+        index = index[0:13]
         pvpc = pvpc + index + ' - ' + str(row[0]) + '-' + '\n'  
     return (pvpc)
 
