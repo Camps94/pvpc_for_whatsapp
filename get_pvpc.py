7617d3a4c0898028d7eb86b687ab7d5a123d8438
Camps94 <eduardocampillos@Eduardos-MacBook-Pro.local>
2022-09-28 01:20:01 +0100

    Update github

--- get_pvpc.py
+++ get_pvpc.py
@@ -337,6 +337,7 @@ def get_pvpc_results():
     df_merged = esios.merge_series(df_list, names)  # merge the DataFrames into a single one
     df = df_merged[names]  # get the actual series and neglect the rest of the info
     pvpc = ''
+    print(df)
     for index, row in df.iterrows():
         if index == df.index[-1]:
             pass
