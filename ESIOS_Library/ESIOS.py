4c575c2fe1ebe045fe2003de0691253b8aa123f8
Camps94 <eduardocampillos@Eduardos-MacBook-Pro.local>
2022-09-29 22:28:25 +0100

    Update github

--- ESIOS_Library/ESIOS.py
+++ ESIOS_Library/ESIOS.py
@@ -336,7 +336,7 @@ class ESIOS(object):
         print(df.columns)
         for index, row in df.iterrows():
             index = index[11:13]
-            if index == df['Término de facturación de energía activa del PVPC 2.0TD'].nsmallest(2).index[0] or 
+            if index == df['Término de facturación de energía activa del PVPC 2.0TD'].nsmallest(2).index[0] or \
             index == df['Término de facturación de energía activa del PVPC 2.0TD'].nsmallest(2).index[1]:
                 pvpc = pvpc + '*' + index + ':00 - ' + str(row[0]) + u" \N{euro sign}/MWh" + '*\n'
             else:
