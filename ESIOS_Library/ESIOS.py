d0c90e38879367b49a10b2088fbf01c423035c76
Camps94 <eduardocampillos@Eduardos-MacBook-Pro.local>
2022-09-29 22:34:48 +0100

    Update github

--- ESIOS_Library/ESIOS.py
+++ ESIOS_Library/ESIOS.py
@@ -333,7 +333,7 @@ class ESIOS(object):
         df_merged = esios.merge_series(df_list, names)  # merge the DataFrames into a single one
         df = df_merged[names]  # get the actual series and neglect the rest of the info
         pvpc = ''
-        print(df.columns)
+        print(df['Término de facturación de energía activa del PVPC 2.0TD'].nsmallest(2))
         for index, row in df.iterrows():
             index = index[11:13]
             if row[0] == df['Término de facturación de energía activa del PVPC 2.0TD'].nsmallest(2).index[0] or \
