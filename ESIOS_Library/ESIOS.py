211d0bc3804ff7feaf87dc8958a406e5754f84e1
Camps94 <eduardocampillos@Eduardos-MacBook-Pro.local>
2022-09-29 22:16:15 +0100

    Update github

--- ESIOS_Library/ESIOS.py
+++ ESIOS_Library/ESIOS.py
@@ -332,8 +332,6 @@ class ESIOS(object):
         df_list, names = esios.get_multiple_series(indicators_, start_, end_)
         df_merged = esios.merge_series(df_list, names)  # merge the DataFrames into a single one
         df = df_merged[names]  # get the actual series and neglect the rest of the info
-        values = df.ix[:,0]
-        df[0].nsmallest(2).index[1]
         pvpc = ''
         print(df)
         for index, row in df.iterrows():
