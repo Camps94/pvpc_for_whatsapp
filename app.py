8b906872befdd6cb5eff9fae7705f0847e22d1ae
Camps94 <eduardocampillos@Eduardos-MacBook-Pro.local>
2022-10-28 23:30:34 +0100

    Sound check

--- app.py
+++ app.py
@@ -6,18 +6,8 @@ import os
 import psycopg2
 from ESIOS_Library.ESIOS import * 
 
-try:
-    from collections.abc import Container, Iterable, MutableSet
-except ImportError:
-    from collections import Container, Iterable, MutableSet
-
-sys.path.insert(1, '/ESIOS_Library')
-ESIOS_CREDENTIAL = os.getenv("ESIOS_CREDENTIAL")
 
 
-
-token = ESIOS_CREDENTIAL
-esios = ESIOS(token)
 
 app = Flask(__name__)
 
@@ -25,32 +15,13 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
-@app.route("/sms2", methods=['POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
-
-    # Create reply
-    resp = MessagingResponse()
-    #var = get_pvpc_results()
-    var = esios.get_pvpc_results(token)
-    today = date.today()
-    d2 = today.strftime("%A, %d %B %Y")
-
-    if msg == "PVPC" or msg == "pvpc":
-        resp.message("PVPC - {}:\n\n{}".format(d2, var))
-    else:
-        resp.message("Hi, You said: {}. If you want to get the PVPC prices for today, reply PVPC. ".format(msg))
-
-    return str(resp)
-
-@app.route("/sms", methods=['POST'])
+@app.route("/ddbb", methods=['POST'])
 def updateDDBB():
+
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     print(request.form)
-    user = request.form.get('To')
+    user = request.form.get('From')
     msg = request.form.get('Body')
 
     # Create reply
--- send_daily_pvpc.py
+++ send_daily_pvpc.py
@@ -1,17 +1,31 @@
 import os
 from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse
+import sys 
+from ESIOS_Library.ESIOS import * 
+from datetime import date
 
 
+sys.path.insert(1, '/ESIOS_Library')
+ESIOS_CREDENTIAL = os.getenv("ESIOS_CREDENTIAL")
+token = ESIOS_CREDENTIAL
+esios = ESIOS(token)
+
+pvpc = esios.get_pvpc_results(token)
+today = date.today()
+today_format = today.strftime("%A, %d %B %Y")
+
+message = "PVPC - {}:\n\n{}".format(today_format, pvpc)
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
+
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Hello there!',
+                              body=message,
                               from_='whatsapp:+34722203982',
-                              to='whatsapp:+34722203982'
+                              to='whatsapp:+34646190000'
                           )
-
 print(message.sid)
